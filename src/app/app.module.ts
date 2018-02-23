a927b18e8f43d3d245d1bbc16eb888dbf37aa111
Benedilson de Jesus  Batista de Lima <benedilson.lima@infosolo.com.br>
2018-02-23 10:32:24 -0300

    CRUD Angular OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { ConfigService } from './services/config.service';
+import { PessoaService } from './services/pessoa.service';
+import { MenuComponnet } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { ConsultaComponent } from './pessoa/consulta/consulta.component';
+import { CadastroComponent } from './pessoa/cadastro/cadastro.component';
+import { routing } from '../app.routes';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponnet,
+    HomeComponent,
+    ConsultaComponent,
+    CadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    routing
   ],
-  providers: [],
+  providers: [ConfigService, PessoaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
